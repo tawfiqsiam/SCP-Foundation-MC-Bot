dcf81ef2ca72934d4527e17730abcd90a406004a
ElektroPrinzYT <44111695+ElektroPrinz@users.noreply.github.com>
2020-04-20 10:22:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
+const discord = requrie('discord.js'); //The disocrd.js package / discord api
 
+const client = new discord.Client(); // The Discord Bot Client
+
+client.on('ready', () => {
+  client.activity_list = [
+    `with my creator anime`,
+    `with BlackDevWolve anime`,
+    `with my creator anime`
+  ]
+  bot.user.setPresence({
+    game: {
+      type: "PLAYING",
+      name: "nichts" + ` | .help`
+    },
+    status: "idle"
+  });
+  
+});
