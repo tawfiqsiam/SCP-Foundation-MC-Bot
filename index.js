268ec0037d9a49943153712d6c4c12dbf550b29e
ElektroPrinzYT <44111695+ElektroPrinz@users.noreply.github.com>
2020-04-20 10:43:35 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,44 @@ client.on('ready', () => {
   ]
   bot.user.setPresence({
     game: {
-      type: "PLAYING",
+      type: "WATCHING",
       name: "nichts" + ` | .help`
     },
     status: "idle"
   });
   
+  setInterval(() => {
+    const index = Math.floor(
+      Math.random() * (bot.activity_list.length - 1) + 1
+    );
+    bot.user.setPresence({
+      game: {
+        type: "WATCHING",
+        name: bot.activity_list[index] + ` | .help`
+      },
+      status: "idle"
+    });
+  }, 10000);
+  
 });
+
+client.on('message', msg => {
+  var prefix = '.';
+  
+  //prefix-check
+  if (!message.content.startsWith(prefix)) return;
+  
+  //definitions
+  const args = msg.content.slice(prefix.length).trim().split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  
+  //Command Starts
+  if (cmd == 'help') {
+    var helpembed = new discord.MessageEmbed()
+    .setTitle('Oops')
+    .setDescription('The helpcommand is in building')
+    .setColor('#eb3333');
+  }
+});
+
+client.login('');
